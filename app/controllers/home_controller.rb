2cd22a45bbed2c79ece73cb7726c4a181ddff5ba
Yaroslav Shmarov <yshmarov@gmail.com>
2024-06-02 23:02:21 +0200

    buttons CSS
    getbootstrap has the best button API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def buttons
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/buttons', to: 'home#buttons'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
